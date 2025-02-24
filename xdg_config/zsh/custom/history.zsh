feae27dd93a44a6a8c2c3506534dc7d24a595539
arkdot <198597192+arkdot@users.noreply.github.com>
2025-02-25 00:01:44 +0100

    better history

--- xdg_config/zsh/custom/history.zsh
+++ xdg_config/zsh/custom/history.zsh
@@ -3,23 +3,23 @@
 # Store date and time in history
 export HISTTIMEFORMAT="%d/%m/%y %T "
 
-# keep history up to date, across sessions, in realtime
-#  http://unix.stackexchange.com/a/48113
-export HISTFILE=$HOME/.zsh_history
-export HISTCONTROL=ignoredups:erasedups                   # no duplicate entries
-export HISTSIZE=100000                                    # big big history (default is 500)
-export HISTFILESIZE=$HISTSIZE                             # big big history
+# History file configuration
+[ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
+HISTCONTROL=ignoredups:erasedups                   # no duplicate entries
+HISTSIZE=100000                                    # big big history (default is 500)
+HISTFILESIZE=$HISTSIZE                             # big big history
 if type shopt &> /dev/null; then shopt -s histappend; fi  # append to history, don't overwrite it
 
 # Save and reload the history after each command finishes
 # The only downside with this is [up] on the readline will go over all history not just this bash session.
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
-setopt hist_expire_dups_first
-setopt hist_ignore_dups
-setopt hist_ignore_space
-setopt hist_verify
-setopt share_history
 bindkey '^[[A' history-search-backward
 bindkey '^[[B' history-search-forward
 
+setopt extended_history       # record timestamp of command in HISTFILE
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt share_history          # share command history data
