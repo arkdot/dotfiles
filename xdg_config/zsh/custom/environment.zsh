39aa596b1094bdcfcf0139e10dc7d4fef44d824a
arkdot <198597192+arkdot@users.noreply.github.com>
2025-10-06 10:04:04 +0200

    move fzf environment variables to environment.zsh

--- xdg_config/zsh/custom/environment.zsh
+++ xdg_config/zsh/custom/environment.zsh
@@ -7,3 +7,15 @@ export EDITOR="nvim"
 export VISUAL="nvim"
 
 prepend_path $HOME/.local/bin
+
+# Setup FZF
+export FZF_DEFAULT_OPTS="
+	--color=fg:#908caa,bg:#191724,hl:#ebbcba
+	--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+	--color=border:#403d52,header:#31748f,gutter:#191724
+	--color=spinner:#f6c177,info:#9ccfd8
+	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
+export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+
